2bfbfbc34534074fb70895aa789c1a07a758fbaf
Luiz Gabriel Mello <148294213+luizgabrielmello@users.noreply.github.com>
2024-05-17 22:30:55 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ function imageForecast(icon) {
     return `img/iconPrevisaoTempo/campoPrevisao/${icon}.png`
 }
 
+
 async function weatherForecast(city) {
     const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${'ba438284fa602652dd54444ff2b38918'}&lang=pt_br&units=metric`).then(result => result.json())
     
